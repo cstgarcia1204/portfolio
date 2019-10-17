bd1e9c356858b6e7d76cce30611860ec55659398
Cinthia <ckstgarcia@gmail.com>
2019-10-17 05:56:17 -0500

    Updates

---
+++ precache-manifest.51694bb7957140576cd9148bb8b36996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2746921ddf6924534c7e80900cac64db",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "202303848034fbfc09d0",
+    "url": "/portfolio/static/css/main.843766da.chunk.css"
+  },
+  {
+    "revision": "af774ed032ca18302085",
+    "url": "/portfolio/static/js/2.5e0bc987.chunk.js"
+  },
+  {
+    "revision": "202303848034fbfc09d0",
+    "url": "/portfolio/static/js/main.201c0374.chunk.js"
+  },
+  {
+    "revision": "effa5f00d4d6774b6f94",
+    "url": "/portfolio/static/js/runtime-main.99e7896e.js"
+  }
+]);
