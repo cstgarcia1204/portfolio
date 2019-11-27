4e68c099ad13f43154bdcec3b951b455414eddd6
Cinthia <ckstgarcia@gmail.com>
2019-11-27 02:09:39 -0600

    Updates

---
+++ precache-manifest.6a548a9d4003d045de2f5bf1be03cb7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265304d957d1a21574dbfb3a10b70192",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba0417df45c9c927581d",
+    "url": "/portfolio/static/css/main.9e8c2129.chunk.css"
+  },
+  {
+    "revision": "a5ad91d011123e52e070",
+    "url": "/portfolio/static/js/2.79dded07.chunk.js"
+  },
+  {
+    "revision": "ba0417df45c9c927581d",
+    "url": "/portfolio/static/js/main.3351f2c1.chunk.js"
+  },
+  {
+    "revision": "effa5f00d4d6774b6f94",
+    "url": "/portfolio/static/js/runtime-main.99e7896e.js"
+  },
+  {
+    "revision": "05d2b6168d5bea96c09a33b974571348",
+    "url": "/portfolio/static/media/purple-polygon-background.05d2b616.png"
+  }
+]);
